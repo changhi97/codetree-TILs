16b5aa891ac4d54fbfd42244d14bd2399263fd3d
changhi97 <77597957+changhi97@users.noreply.github.com>
2024-10-07 22:56:45 +0900

    Title: 고대 문명 유적 탐사, Time: 134ms, Memory: 11MB, Status: Passed - CodeTree

--- 241007/고대 문명 유적 탐사/ancient-ruin-exploration.java
+++ 241007/고대 문명 유적 탐사/ancient-ruin-exploration.java
@@ -2,27 +2,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 public class Main {
-    static int N, K, M;
+    static int N, K, M, mIdx;
+
     static int[][] board;
-    static int mPoint = 0;
     static int[] wall;
     static int[] dx = {1, 0, -1, -1, -1, 0, 1, 1}, dy = {-1, -1, -1, 0, 1, 1, 1, 0};
-    static int[] dxB = {0, -1, 0, 1}, dyB = {-1, 0, 1, 0};
+
+    static int[] dxW = {-1, 0, 1, 0}, dyW = {0, -1, 0, 1};
     static int[] round;
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
 
         st = new StringTokenizer(br.readLine());
         N = 5;
         K = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        mIdx = 0;
 
         board = new int[N][N];
+        wall = new int[M];
+        round = new int[K];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
@@ -30,18 +36,15 @@ public class Main {
             }
         }
 
-        wall = new int[M];
-        round = new int[K];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             wall[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 0; i < K; i++) {
-            if (!sol(i)) break;
+        StringBuilder answer = new StringBuilder();
+        for(int i =0; i<K; i++){
+            if(!sol(i)) break;
         }
-
-        StringBuilder answer=  new StringBuilder();
 
         for(int i : round){
             if(i==0) break;
@@ -49,105 +52,80 @@ public class Main {
         }
         System.out.println(answer);
 
+
     }
 
     public static boolean sol(int id) {
-
-        int max = 0;
-        Node maxNode = null;
-        int maxRotate = 4;
-
+        int maxCount = 0;
+        Node node = null;
+        int minRotate = 4;
 
         for (int i = 1; i < 4; i++) {
             for (int j = 1; j < 4; j++) {
 
-                for (int r = 0; r < 3; r++) {
+                for (int r = 1; r <= 3; r++) {
                     rotateRight(j, i);
 
                     boolean[][] v = new boolean[N][N];
                     int count = 0;
-
                     for (int x = 0; x < N; x++) {
                         for (int y = 0; y < N; y++) {
                             if (!v[x][y]) {
-                                int result = bfs(v, x, y);
-                                count+=result;
+                                int tempCount = findPiece(v, x, y);
+                                if (tempCount >= 3) count += tempCount;
                             }
                         }
                     }
 
+                    if (count > maxCount) {
+                        maxCount = count;
+                        minRotate = r;
+                        node = new Node(j, i);
 
-//                    if(max>count) continue;
-//                    if (max < count
-//                            || (max != 0 && max==count && maxRotate>r+1)
-//                            || maxNode==null
-//                            || (maxNode.y>j || ( maxNode.y==j && maxNode.x>i))) {
-//                        max = count;
-//
-//
-//                        maxNode = new Node(i, j);
-//                        maxRotate = r+1;
-//                    }
-
-                    if(max<count){
-                        max=count;
-                        maxNode = new Node(j,i);
-                        maxRotate = r+1;
-                    }else if(max==count && maxRotate>r+1){
-                        maxNode=new Node(j,i);
-                        maxRotate=r+1;
+                    } else if (count == maxCount && minRotate > r) {
+                        minRotate = r;
+                        node = new Node(j, i);
                     }
                 }
 
-                for (int r = 0; r < 3; r++) {
+                for (int r = 1; r <= 3; r++) {
                     rotateLeft(j, i);
                 }
             }
         }
 
-        if (max == 0) return false;
+        if (maxCount == 0) return false;
 
-
-        for (int r = 0; r < maxRotate; r++) {
-            rotateRight(maxNode.x, maxNode.y);
+        for(int i =1; i<=minRotate; i++){
+            rotateRight(node.x,node.y);
         }
 
-//        print();
-        while(true){
-            int countSum=0;
+        while (true) {
+            int count = 0;
             boolean[][] v= new boolean[N][N];
-            for(int i =0; i<N; i++){
+            for (int i = 0; i<N; i++){
                 for(int j =0; j<N; j++){
-                    if(!v[i][j] && bfs(v,i,j) > 0 && board[i][j]!=0){
-                        int result  = select(i,j);
-
-                        countSum+=result;
-
+                    if(!v[i][j] && findPiece(v,i,j)>=3){
+                        count+=getPiece(i,j);
                     }
-
                 }
             }
-
-//            System.out.println(countSum);
-
-            if(countSum==0) break;
-            round[id]+=countSum;
-//            System.out.println("M" +" "+mPoint);
-//            System.out.println();
-//            print();
+            if(count==0) break;
+            round[id] += count;
             fillBoard();
-//            print();
+
         }
-
-//        print();
-//        answer += select(maxNode.x, maxNode.y);
-
-//        fillBoard();
-
-
         return true;
+    }
 
-
+    public static void fillBoard(){
+        for(int i =0; i<N; i++){
+            for(int j =N-1; j>=0; j--){
+                if(board[j][i]==0){
+                    board[j][i] = wall[mIdx++];
+                }
+            }
+        }
     }
 
     public static void rotateRight(int x, int y) {
@@ -158,14 +136,15 @@ public class Main {
             temp.add(board[nx][ny]);
         }
 
+
         for (int i = 0; i < 8; i++) {
             int nx = x + dx[(i + 2) % 8];
             int ny = y + dy[(i + 2) % 8];
             board[nx][ny] = temp.get(i);
         }
 
-    }
 
+    }
 
     public static void rotateLeft(int x, int y) {
         List<Integer> temp = new ArrayList<>();
@@ -180,87 +159,70 @@ public class Main {
             int ny = y + dy[(i + 2) % 8];
             board[nx][ny] = temp.get(i);
         }
-
     }
 
-    public static int bfs(boolean[][] v, int x, int y) {
-        Queue<Node> q = new LinkedList<>();
 
+    public static int findPiece(boolean[][] v, int x, int y) {
+        Queue<Node> q = new LinkedList<>();
         q.offer(new Node(x, y));
         v[x][y] = true;
+        int id = board[x][y];
         int count = 1;
-
         while (!q.isEmpty()) {
             Node node = q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nx = node.x + dxB[i];
-                int ny = node.y + dyB[i];
+                int nx = node.x + dxW[i];
+                int ny = node.y + dyW[i];
 
                 if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
 
-                if (!v[nx][ny] && board[x][y] == board[nx][ny]) {
+                if (!v[nx][ny] && board[nx][ny] == id) {
                     v[nx][ny] = true;
                     count++;
                     q.offer(new Node(nx, ny));
                 }
 
             }
-        }
 
-        if (count < 3) count = 0;
+        }
         return count;
     }
 
-    public static int select(int x, int y) {
+    public static int getPiece(int x, int y) {
         Queue<Node> q = new LinkedList<>();
         q.offer(new Node(x, y));
-        boolean[][] v = new boolean[N][N];
-        v[x][y] = true;
+        boolean[][] v=new boolean[N][N];
+        int id = board[x][y];
         int count = 1;
+
+        v[x][y] = true;
+        board[x][y]=0;
 
         while (!q.isEmpty()) {
             Node node = q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nx = node.x + dxB[i];
-                int ny = node.y + dyB[i];
+                int nx = node.x + dxW[i];
+                int ny = node.y + dyW[i];
 
                 if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
 
-                if (!v[nx][ny] && board[x][y] == board[nx][ny]) {
-                    board[nx][ny] = 0;
+                if (!v[nx][ny] && board[nx][ny] == id) {
                     v[nx][ny] = true;
+                    board[nx][ny]=0;
                     count++;
                     q.offer(new Node(nx, ny));
                 }
 
             }
+
         }
-        board[x][y] =0;
         return count;
-
     }
-
-//    // (1) 열 번호가 작은 순으로 조각이 생겨납니다.
-//    // 만약 열 번호가 같다면 (2) 행 번호가 큰 순으로 조각이 생겨납니다.
-    public static void fillBoard() {
-        for (int i = 0; i < N; i++) {
-            for (int j = N - 1; j >= 0; j--) {
-                if (board[j][i] == 0) {
-                    board[j][i] = wall[mPoint++];
-                }
-
-            }
-        }
-    }
-
-
 
     public static void print() {
-        for (int i = 0; i < N; i++) {
-            System.out.println(Arrays.toString(board[i]));
-        }
+        for (int i = 0; i < N; i++) System.out.println(Arrays.toString(board[i]));
         System.out.println();
     }
 
@@ -272,5 +234,4 @@ public class Main {
             this.y = y;
         }
     }
-
 }
